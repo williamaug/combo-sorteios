f6080336f2556778defe70fc6db1b6797a9ac8bd
William RIbeiro <145608917+williamaug@users.noreply.github.com>
2023-10-25 09:15:03 -0400

    Delete sorteioadmscript.js

--- sorteioadmscript.js
+++
@@ -1,9 +0,0 @@
-let form = document.getElementById('form')
-let rol = document.getElementById('roleta')
-const numeroAleatorio = Math.floor(Math.random() * 100);
-if(String(form.innerText) == "Digital"){
-    rol.innerHTML = '<button id="rolar" onclick="rolar()"> ROLAR NUMERO </button>'
-}
-function rolar() {
-    rol.innerHTML += `<p>${numeroAleatorio}</p>`
-}
